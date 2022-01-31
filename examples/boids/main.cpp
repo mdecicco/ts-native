416fc8d089a5105f91c015bba0b0067abaed7df4
Dr. Michael DeCicco <mdecicco8@gmail.com>
2022-01-31 01:56:02 -0500

    .

--- examples/boids/main.cpp
+++ examples/boids/main.cpp
@@ -1,6 +1,7 @@
 #include <gjs/gjs.h>
 #include <gjs/builtin/script_vec2.h>
 #include <gjs/builtin/script_vec3.h>
+#include <gjs/gjs.hpp>
 
 #include <stdio.h>
 #include <time.h>
@@ -156,7 +157,7 @@ int main(int arg_count, const char** args) {
 
     script_module* mod = ctx.resolve("boids");
     if (!mod) {
-        print_log(&ctx);
+        //print_log(&ctx);
         return -1;
     }
     // print_code(&be);
