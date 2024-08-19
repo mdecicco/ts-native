b22e41bef55f8b8979b8e090970460b33304db87
Michael <mdecicco8@gmail.com>
2024-08-19 08:54:51 -0400

    fix tsnr compilation error handling

--- tools/tsnr/tsnr.cpp
+++ tools/tsnr/tsnr.cpp
@@ -328,9 +328,11 @@ i32 processConfig(const json& configIn, Config& configOut, const tsnc_config& ts
 }
 
 i32 handleResult(Context* ctx, Module* mod, const tsnc_config& conf) {
-    bool hadErrors = ctx->getPipeline()->getLogger()->hasErrors();
+    compiler::Logger* log = ctx->getPipeline()->getLogger();
+    if (!log) return mod ? COMPILATION_SUCCESS : COMPILATION_ERROR;
 
-    const auto& logs = ctx->getPipeline()->getLogger()->getMessages();
+    bool hadErrors = log->hasErrors();
+    const auto& logs = log->getMessages();
     constexpr const char* logTps[] = { "debug", "info", "warning", "error" };
 
     for (u32 i = 0;i < logs.size();i++) {
